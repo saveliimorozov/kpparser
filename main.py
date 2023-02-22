370aa178661476e87bd9761af0355d11492ee301
saveliimorozov <savkamorozov7@gmail.com>
2023-02-22 21:09:42 +0300

    Get NameRU + order

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import requests as req
 from bs4 import BeautifulSoup as bs
 from fake_useragent import UserAgent
@@ -5,25 +7,44 @@ from fake_useragent import UserAgent
 url = 'https://www.kinopoisk.ru/lists/movies/top-250-2020/'
 
 
-def prepare(url: str):
+def getSitePageInText(url: str):
     urlReq = req.get(url, headers={'User-Agent': UserAgent().chrome})
-    # print(urlReq)
+    print(urlReq)
+    time.sleep(1)
 
     soupUrlReq = bs(urlReq.text, 'lxml')
-    # print(soupUrlReq.prettify())
+    print(soupUrlReq)
 
-    movieMainInfo = soupUrlReq.find('div', class_='styles_root__ti07r').find(
-        'a', class_='base-movie-main-info_link__YwtP1')
+    return soupUrlReq
+
+
+
+
+
+def getMovieMainInfo(soupUrlReq):
+
+    movieMainInfo = soupUrlReq.find('div', class_="styles_root__ti07r").find(
+        'a', class_="base-movie-main-info_link__YwtP1")
 
     movieLink = 'https://www.kinopoisk.ru' + movieMainInfo.get('href')
 
     movieNameRu = movieMainInfo.find('div', class_='base-movie-main-info_mainInfo__ZL_u3').find(
         'span', class_='styles_mainTitle__IFQyZ styles_activeMovieTittle__kJdJj').text
 
+    movieNameOrig = movieMainInfo.find('div', class_='desktop-list-main-info_secondaryTitleSlot__mc0mI').find(
+        'span', class_='desktop-list-main-info_secondaryTitle__ighTt').text
+
+    movieYearandDuration = movieMainInfo.find(
+        'span', class_="desktop-list-main-info_secondaryText__M_aus").contents[2]
+
+
+
     print(movieMainInfo.prettify())
     print(movieLink)
     print(movieNameRu)
+    print(movieNameOrig)
+    print(movieYearandDuration)
 #
 
 if __name__ == '__main__':
-    prepare(url)
+    getMovieMainInfo(getSitePageInText(url))
