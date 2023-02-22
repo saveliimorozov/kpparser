cb2d95d94f964372a7b63130830c603915836c07
saveliimorozov <savkamorozov7@gmail.com>
2023-02-22 16:59:38 +0300

    Get movie link

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ url = 'https://www.kinopoisk.ru/lists/movies/top-250-2020/'
 def prepare(url: str):
     urlReq = req.get(url)
     soupUrlReq = bs(urlReq.text, 'lxml')
-    movieClass = soupUrlReq.find('div', class_='styles_root__ti07r')
-    print(movieClass)
+    movieLink = 'https://www.kinopoisk.ru' + soupUrlReq.find(
+        'div', class_='styles_root__ti07r').find(
+        'a', class_='base-movie-main-info_link__YwtP1').get('href')
+    print(movieLink)
 
 
 if __name__ == '__main__':
