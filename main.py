33a6f219cf20e32aba4b9cc0b6a3e9fbdaa523e9
saveliimorozov <savkamorozov7@gmail.com>
2023-02-22 16:47:15 +0300

    Get first movieClass

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import requests as req
+from bs4 import BeautifulSoup as bs
+
+url = 'https://www.kinopoisk.ru/lists/movies/top-250-2020/'
 
 
-def prepare():
-    pass
-
-
+def prepare(url: str):
+    urlReq = req.get(url)
+    soupUrlReq = bs(urlReq.text, 'lxml')
+    movieClass = soupUrlReq.find('div', class_='styles_root__ti07r')
+    print(movieClass)
 
 
 if __name__ == '__main__':
-    prepare()
-    print('Your first test')
-
-
+    prepare(url)
